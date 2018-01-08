d3f522e3f0139281d5f66c0547bb6f3081463381
Santiago Martín Agra <santi@code.local>
2018-01-08 22:25:08 +0100

    feat(FooterInfo): Logos de impulsa y startup weekend asturias

---
+++ src/components/FooterInfo.vue
@@ -0,0 +1,45 @@
+<template>
+  <footer>
+    <ul>
+      <li>
+        <span>
+          Financiados por:
+        </span>
+        <a href="http://impulsa.gijon.es">
+          <img src="../assets/logo_gijon_impulsa.png">
+        </a>
+      </li>
+      <li>
+        <span>
+          Ganadores:
+        </span>
+        <a href="http://www.elcomercio.es/gijon/proyecto-deporte-compania-20171031002039-ntvo.html">
+          <img src="../assets/logo_startup_weekend.png">
+        </a>
+      </li>
+    </ul>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'footer-info'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+footer{
+  padding: 10px;
+}
+footer img{
+  width: auto;
+  max-height: 40px;
+  vertical-align: middle
+}
+li{
+  list-style: none;
+  display: inline;
+  margin-left: 10px;
+}
+</style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,6 +2,7 @@
   <div>
     <carousel></carousel>
     <form-test></form-test>
+    <footer-info></footer-info>
   </div>
 </template>
 
@@ -9,12 +10,14 @@
 
 import Carousel from './Carousel.vue'
 import FormTest from './FormTest.vue'
+import FooterInfo from './FooterInfo.vue'
 
 export default {
   name: 'home',
   components: {
     'carousel': Carousel,
-    'form-test': FormTest
+    'form-test': FormTest,
+    'footer-info': FooterInfo
   }
 }
 </script>
