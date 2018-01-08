3aa749bdea46ca6581833d660fe9fa5e36b779d6
Santiago Martín Agra <santi@code.local>
2018-01-08 22:13:37 +0100

    feat(HomeComponent): Añadido FormTestComponent con el formulario

---
+++ src/components/FormTest.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="footer-form">
+    <h1>
+      ¡Cuentanos algo sobre ti!
+    </h1>
+    <div class="padding-form">
+      <label for="name">
+        Nombre:
+      </label>
+      <input id="name">
+    </div>
+    <div class="padding-form">
+      <label for="age">
+        Edad
+      </label>
+      <input id="age" type="number">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'form-test'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+.footer-form{
+  padding: 10px;
+  text-align: center;
+}
+.padding-form{
+  padding: 10px;
+}
+</style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,17 +1,20 @@
 <template>
   <div>
     <carousel></carousel>
+    <form-test></form-test>
   </div>
 </template>
 
 <script>
 
 import Carousel from './Carousel.vue'
+import FormTest from './FormTest.vue'
 
 export default {
   name: 'home',
   components: {
     'carousel': Carousel,
+    'form-test': FormTest
   }
 }
 </script>
