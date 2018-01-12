d50c92875c570c7f7e8b0ff26849078fc11512c8
Santiago Martín Agra <santi@code.local>
2018-01-12 18:47:36 +0100

    feat(AppDescripcion): Tracking Google Analytics

--- src/components/AppDescription.vue
+++ src/components/AppDescription.vue
@@ -13,16 +13,16 @@
         <p class="p-s-resp italic c-primary playfair">“Encuentra a gente en tu ciudad para practicar deporte a través de una app móvil”.</p>
         <div class="row">
           <div class="col-xs-6">
-            <a class="padding-16 display-ib" target="_blank" href="https://twitter.com/SWAsturias/status/924687764645470208"><img class="width-0100" v-lazy="'https://pbs.twimg.com/media/DMR6vHqX4AADjnw.png'" alt="Twitter"></a>
+            <a v-on:click="gaNewsTracking('SWAsturias')" class="padding-16 display-ib" target="_blank" href="https://twitter.com/SWAsturias/status/924687764645470208"><img class="width-0100" v-lazy="'https://pbs.twimg.com/media/DMR6vHqX4AADjnw.png'" alt="Twitter"></a>
           </div>
           <div class="col-xs-6">
-            <a class="padding-16 display-ib" target="_blank" href="http://www.rtpa.es/asturias:Convertir-una-idea-en-una-empresa-que-resuelva-las-necesidades-ciudadanas_111509302781.html"><img class="width-0100" v-lazy="'http://media.tvalacarta.info/canales/256x256/rtpa.png'" alt="RTPA"></a>
+            <a v-on:click="gaNewsTracking('rtpa')" class="padding-16 display-ib" target="_blank" href="http://www.rtpa.es/asturias:Convertir-una-idea-en-una-empresa-que-resuelva-las-necesidades-ciudadanas_111509302781.html"><img class="width-0100" v-lazy="'http://media.tvalacarta.info/canales/256x256/rtpa.png'" alt="RTPA"></a>
           </div>
           <div class="col-xs-6">
-            <a class="padding-16 display-ib" target="_blank" href="http://www.lne.es/economia/2017/11/05/aplicaciones-ponerse-correr/2188255.html"><img class="width-0100" v-lazy="'https://mas.lne.es/portadas/img/lanuevaespana.png?aaa'" alt="La nueva españa"></a>
+            <a v-on:click="gaNewsTracking('lne')" class="padding-16 display-ib" target="_blank" href="http://www.lne.es/economia/2017/11/05/aplicaciones-ponerse-correr/2188255.html"><img class="width-0100" v-lazy="'https://mas.lne.es/portadas/img/lanuevaespana.png?aaa'" alt="La nueva españa"></a>
           </div>
           <div class="col-xs-6">
-            <a class="padding-16 display-ib" target="_blank" href="http://www.elcomercio.es/gijon/proyecto-deporte-compania-20171031002039-ntvo.html"><img class="width-0100" v-lazy="'http://static1.elcomercio.es/squido/latest/assets/images/elcomercio.svg'" alt="El comercio"></a>
+            <a v-on:click="gaNewsTracking('elcomercio')" class="padding-16 display-ib" target="_blank" href="http://www.elcomercio.es/gijon/proyecto-deporte-compania-20171031002039-ntvo.html"><img class="width-0100" v-lazy="'http://static1.elcomercio.es/squido/latest/assets/images/elcomercio.svg'" alt="El comercio"></a>
           </div>
         </div>
       </div>
@@ -42,73 +42,73 @@
       </div>
     <div class="row padding-t-30">
       <div class="col-xs-6 col-sm-4 col-md-3 col-lg-2">
-          <div class="icon-text-box cursor-p">
+          <div v-on:click="gaSportTracking('padel')" class="icon-text-box cursor-p">
             <img src="../assets/icons/padel.png" alt="padel icon">
             <p class="c-primary">Pádel</p>
           </div>
       </div>
       <div class="col-xs-6 col-sm-4 col-md-3 col-lg-2">
-          <div class="icon-text-box cursor-p">
+          <div v-on:click="gaSportTracking('futbol')" class="icon-text-box cursor-p">
             <img src="../assets/icons/futbol.png" alt="futbol icon">
             <p class="c-primary">Futbol</p>
           </div>
       </div>
       <div class="col-xs-6 col-sm-4 col-md-3 col-lg-2">
-          <div class="icon-text-box cursor-p">
+          <div v-on:click="gaSportTracking('balonces')" class="icon-text-box cursor-p">
             <img src="../assets/icons/baloncesto.png" alt="baloncesto icon">
             <p class="c-primary">Baloncesto</p>
           </div>
       </div>
       <div class="col-xs-6 col-sm-4 col-md-3 col-lg-2">
-          <div class="icon-text-box cursor-p">
+          <div v-on:click="gaSportTracking('caminar')" class="icon-text-box cursor-p">
             <img src="../assets/icons/caminar.png" alt="caminar icon">
             <p class="c-primary">Caminar</p>
           </div>
       </div>
       <div class="col-xs-6 col-sm-4 col-md-3 col-lg-2">
-          <div class="icon-text-box cursor-p">
+          <div v-on:click="gaSportTracking('surf')" class="icon-text-box cursor-p">
             <img src="../assets/icons/surf.png" alt="surf icon">
             <p class="c-primary">Surf</p>
           </div>
       </div>
       <div class="col-xs-6 col-sm-4 col-md-3 col-lg-2">
-          <div class="icon-text-box cursor-p">
+          <div v-on:click="gaSportTracking('tenis')" class="icon-text-box cursor-p">
             <img src="../assets/icons/tenis.png" alt="tenis icon">
             <p class="c-primary">Tenis</p>
           </div>
       </div>
       <div class="col-xs-6 col-sm-4 col-md-3 col-lg-2">
-          <div class="icon-text-box cursor-p">
+          <div v-on:click="gaSportTracking('bolos')" class="icon-text-box cursor-p">
             <img src="../assets/icons/bolos.png" alt="bolos icon">
             <p class="c-primary">Bolos</p>
           </div>
       </div>
       <div class="col-xs-6 col-sm-4 col-md-3 col-lg-2">
-          <div class="icon-text-box cursor-p">
+          <div v-on:click="gaSportTracking('correr')" class="icon-text-box cursor-p">
             <img src="../assets/icons/correr.png" alt="correr icon">
             <p class="c-primary">Correr</p>
           </div>
       </div>
       <div class="col-xs-6 col-sm-4 col-md-3 col-lg-2">
-          <div class="icon-text-box cursor-p">
+          <div v-on:click="gaSportTracking('billar')" class="icon-text-box cursor-p">
             <img src="../assets/icons/billar.png" alt="billar icon">
             <p class="c-primary">Billar</p>
           </div>
       </div>
       <div class="col-xs-6 col-sm-4 col-md-3 col-lg-2">
-          <div class="icon-text-box cursor-p">
+          <div v-on:click="gaSportTracking('skating')" class="icon-text-box cursor-p">
             <img src="../assets/icons/skating.png" alt="skating icon">
             <p class="c-primary">Skating</p>
           </div>
       </div>
       <div class="col-xs-6 col-sm-4 col-md-3 col-lg-2">
-          <div class="icon-text-box cursor-p">
+          <div v-on:click="gaSportTracking('patinaje')" class="icon-text-box cursor-p">
             <img src="../assets/icons/patinaje.png" alt="patinaje icon">
             <p class="c-primary">Patinaje</p>
           </div>
       </div>
       <div class="col-xs-6 col-sm-4 col-md-3 col-lg-2">
-          <div class="icon-text-box cursor-p">
+          <div v-on:click="gaSportTracking('voleibol')" class="icon-text-box cursor-p">
             <img src="../assets/icons/voleibol.png" alt="voleibol icon">
             <p class="c-primary">Voleibol</p>
           </div>
@@ -119,7 +119,15 @@
 </template>
 <script>
 export default {
-  name: 'AppDescription'
+  name: 'AppDescription',
+  methods: {
+    gaSportTracking: function (sport) {
+      this.$ga.event('Deportes', 'click', sport, 1)
+    },
+    gaNewsTracking: function (media) {
+      this.$ga.event('Enlaces', 'click', media, 1)
+    }
+  }
 }
 </script>
 <style lang="scss">
@@ -240,7 +248,7 @@ $grid-breakpoints: (xs: 0, sm: 576px, md: 768px, lg: 992px, xl: 1200px) !default
 
 .cursor-p {
   cursor: pointer;
-} 
+}
 
 .p-s-resp {
   font-size: 18px;
--- src/main.js
+++ src/main.js
@@ -1,16 +1,26 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import VueAgile from 'vue-agile'
+// import VueAgile from 'vue-agile'
 import VueLazyLoader from 'vue-lazy-loader'
+import VueAnalytics from 'vue-analytics'
 
 import App from './App'
 import router from './router'
 
+Vue.use(VueAnalytics, {
+  id: 'UA-XXX-X'
+})
+
 Vue.config.productionTip = false
 
 Vue.directive('lazy', VueLazyLoader)
-Vue.use(VueAgile)
+// Vue.use(VueAgile)
+
+Vue.use(VueAnalytics, {
+  id: 'UA-109251415-1',
+  checkDuplicatedScript: true
+})
 
 /* eslint-disable no-new */
 new Vue({
