1615ff301494f3a18729438dae67194fe25d50ba
Alvaro Herrero <alvaroherrero@MacBook-Pro-de-Alvaro.local>
2018-01-12 18:28:51 +0100

    cambios descripción

--- src/components/AppDescription.vue
+++ src/components/AppDescription.vue
@@ -2,89 +2,113 @@
   <div class="container">
     <div class="row">
       <div class="col-xs-12 col-md-6">
-        <h1 class="title montserrat c-primary">
+        <h1 class="x-bold title montserrat c-primary white-space-nw">
           Haz más
         </h1>
         <div class="flex-in-line">
-          <img src="../assets/icons/padel.png" alt="padel icon">
-          <p class="title-2 playfair c-secondary">Pádel</p>
+          <img class="icon-size-resp padding-r-16" src="../assets/icons/padel.png" alt="padel icon">
+          <p class="title-2 italic x-bold playfair c-secondary">Pádel</p>
         </div>
-        <p class="c-primary montserrat">“Haz más” Proyecto que nació tras ganar el primer Startup Weekend de Gijón. Empezó como una propuesta sencilla: </p>
-        <p class="c-primary playfair">“Encuentra a gente en tu ciudad para practicar deporte a través de una app móvil”.</p>
+        <p class="p-s-resp c-primary montserrat light">“Haz más” Proyecto que nació tras ganar el primer <span class="bold c-secondary">Startup Weekend de Gijón</span>. Empezó como una propuesta sencilla: </p>
+        <p class="p-s-resp italic c-primary playfair">“Encuentra a gente en tu ciudad para practicar deporte a través de una app móvil”.</p>
+        <div class="row">
+          <div class="col-xs-6">
+            <a class="padding-16 display-ib" target="_blank" href="https://twitter.com/SWAsturias/status/924687764645470208"><img class="width-0100" v-lazy="'https://pbs.twimg.com/media/DMR6vHqX4AADjnw.png'" alt="Twitter"></a>
+          </div>
+          <div class="col-xs-6">
+            <a class="padding-16 display-ib" target="_blank" href="http://www.rtpa.es/asturias:Convertir-una-idea-en-una-empresa-que-resuelva-las-necesidades-ciudadanas_111509302781.html"><img class="width-0100" v-lazy="'http://media.tvalacarta.info/canales/256x256/rtpa.png'" alt="RTPA"></a>
+          </div>
+          <div class="col-xs-6">
+            <a class="padding-16 display-ib" target="_blank" href="http://www.lne.es/economia/2017/11/05/aplicaciones-ponerse-correr/2188255.html"><img class="width-0100" v-lazy="'https://mas.lne.es/portadas/img/lanuevaespana.png?aaa'" alt="La nueva españa"></a>
+          </div>
+          <div class="col-xs-6">
+            <a class="padding-16 display-ib" target="_blank" href="http://www.elcomercio.es/gijon/proyecto-deporte-compania-20171031002039-ntvo.html"><img class="width-0100" v-lazy="'http://static1.elcomercio.es/squido/latest/assets/images/elcomercio.svg'" alt="El comercio"></a>
+          </div>
+        </div>
       </div>
       <div class="col-xs-12 col-md-6 text-align-c">
         <img class="margin-0-auto" src="../assets/icons/vista-app-1.png" alt="vista de la app">
       </div>
      </div>
-     <div class="row">
+     <div class="row padding-t-30">
+       <div class="col-xs-12">
+         <p class="x-bold title montserrat c-primary">
+          Háblanos de ti
+        </p>
+        <p class="title-2 italic x-bold playfair c-secondary">
+          ¿Qué deportes te gustaría practicar?
+        </p>
+       </div>
+      </div>
+    <div class="row padding-t-30">
       <div class="col-xs-6 col-sm-4 col-md-3 col-lg-2">
-          <div class="icon-text-box">
+          <div class="icon-text-box cursor-p">
             <img src="../assets/icons/padel.png" alt="padel icon">
             <p class="c-primary">Pádel</p>
           </div>
       </div>
       <div class="col-xs-6 col-sm-4 col-md-3 col-lg-2">
-          <div class="icon-text-box">
+          <div class="icon-text-box cursor-p">
             <img src="../assets/icons/futbol.png" alt="futbol icon">
             <p class="c-primary">Futbol</p>
           </div>
       </div>
       <div class="col-xs-6 col-sm-4 col-md-3 col-lg-2">
-          <div class="icon-text-box">
+          <div class="icon-text-box cursor-p">
             <img src="../assets/icons/baloncesto.png" alt="baloncesto icon">
             <p class="c-primary">Baloncesto</p>
           </div>
       </div>
       <div class="col-xs-6 col-sm-4 col-md-3 col-lg-2">
-          <div class="icon-text-box">
+          <div class="icon-text-box cursor-p">
             <img src="../assets/icons/caminar.png" alt="caminar icon">
             <p class="c-primary">Caminar</p>
           </div>
       </div>
       <div class="col-xs-6 col-sm-4 col-md-3 col-lg-2">
-          <div class="icon-text-box">
+          <div class="icon-text-box cursor-p">
             <img src="../assets/icons/surf.png" alt="surf icon">
             <p class="c-primary">Surf</p>
           </div>
       </div>
       <div class="col-xs-6 col-sm-4 col-md-3 col-lg-2">
-          <div class="icon-text-box">
+          <div class="icon-text-box cursor-p">
             <img src="../assets/icons/tenis.png" alt="tenis icon">
             <p class="c-primary">Tenis</p>
           </div>
       </div>
       <div class="col-xs-6 col-sm-4 col-md-3 col-lg-2">
-          <div class="icon-text-box">
+          <div class="icon-text-box cursor-p">
             <img src="../assets/icons/bolos.png" alt="bolos icon">
             <p class="c-primary">Bolos</p>
           </div>
       </div>
       <div class="col-xs-6 col-sm-4 col-md-3 col-lg-2">
-          <div class="icon-text-box">
+          <div class="icon-text-box cursor-p">
             <img src="../assets/icons/correr.png" alt="correr icon">
             <p class="c-primary">Correr</p>
           </div>
       </div>
       <div class="col-xs-6 col-sm-4 col-md-3 col-lg-2">
-          <div class="icon-text-box">
+          <div class="icon-text-box cursor-p">
             <img src="../assets/icons/billar.png" alt="billar icon">
             <p class="c-primary">Billar</p>
           </div>
       </div>
       <div class="col-xs-6 col-sm-4 col-md-3 col-lg-2">
-          <div class="icon-text-box">
+          <div class="icon-text-box cursor-p">
             <img src="../assets/icons/skating.png" alt="skating icon">
             <p class="c-primary">Skating</p>
           </div>
       </div>
       <div class="col-xs-6 col-sm-4 col-md-3 col-lg-2">
-          <div class="icon-text-box">
+          <div class="icon-text-box cursor-p">
             <img src="../assets/icons/patinaje.png" alt="patinaje icon">
             <p class="c-primary">Patinaje</p>
           </div>
       </div>
       <div class="col-xs-6 col-sm-4 col-md-3 col-lg-2">
-          <div class="icon-text-box">
+          <div class="icon-text-box cursor-p">
             <img src="../assets/icons/voleibol.png" alt="voleibol icon">
             <p class="c-primary">Voleibol</p>
           </div>
@@ -99,84 +123,137 @@ export default {
 }
 </script>
 <style lang="scss">
-$grid-breakpoints: (
-    xs: 0,
-    sm: 576px,
-    md: 768px,
-    lg: 992px,
-    xl: 1200px
-  ) !default;
+$grid-breakpoints: (xs: 0, sm: 576px, md: 768px, lg: 992px, xl: 1200px) !default;
 
-  h1.title {
-    font-size: 50px;
-    font-weight: 900;
-    text-transform: uppercase;
-    display: block;
-    width: 70%;
-    left: 50%;
-    margin: 0; 
-    white-space: nowrap;
-    @media (min-width: map-get($grid-breakpoints, sm)) {
-        font-size: 70px;
-     }
-    @media (min-width: map-get($grid-breakpoints, md)) {
-      font-size: 90px;
-    }
+.title {
+  font-size: 50px;
+  text-transform: uppercase;
+  display: block;
+  width: 70%;
+  left: 50%;
+  margin: 0;
+  line-height: 50pt;
+  @media (min-width: map-get($grid-breakpoints, sm)) {
+    font-size: 70px;
+    line-height: 70pt;
   }
-  .title-2{
-    font-size: 30px;
-    font-weight: 900;
-    font-style: italic;
-    display: block;
-    width: 70%;
-    left: 50%;
-    margin: 0; 
-    @media (min-width: map-get($grid-breakpoints, sm)) {
-        font-size: 45px;
-     }
-    @media (min-width: map-get($grid-breakpoints, md)) {
+  @media (min-width: map-get($grid-breakpoints, md)) {
+    font-size: 90px;
+    line-height: 90pt;
+  }
+}
+.title-2 {
+  font-size: 30px;
+  display: block;
+  width: 70%;
+  left: 50%;
+  margin: 0;
+  @media (min-width: map-get($grid-breakpoints, sm)) {
+    font-size: 45px;
+  }
+  @media (min-width: map-get($grid-breakpoints, md)) {
     font-size: 60px;
   }
-  }
-  .flex-in-line {
-    display: flex;
-    align-items: center;
-  }
+}
+.flex-in-line {
+  display: flex;
+  align-items: center;
+}
 
-  .icon-text-box {
-    display: flex; 
-    align-items: center;
-    justify-content: center;
-    flex-direction: column;
-    min-height: 200px;
-    &:hover {
-      background: #FFFFFF;
-      box-shadow: 0 2px 20px 0 rgba(0,0,0,0.10);
-      border-radius: 10px;
-    }
+.icon-text-box {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  min-height: 200px;
+  &:hover {
+    background: #ffffff;
+    box-shadow: 0 2px 20px 0 rgba(0, 0, 0, 0.1);
+    border-radius: 10px;
   }
+}
 
-  .montserrat {
-    font-family: 'Montserrat', sans-serif;
-  }
+.x-bold {
+  font-weight: 900;
+}
 
-  .playfair {
-    font-family: 'Playfair', serif;
-  }
+.bold {
+  font-weight: 700;
+}
+.light {
+  font-weight: 300;
+}
 
-  .c-secondary {
-    color: #F93762;
-  }
+.montserrat {
+  font-family: "Montserrat", sans-serif;
+}
 
-  .c-primary {
-    color: #392283;    
-  }
+.playfair {
+  font-family: "Playfair", serif;
+}
 
-  .text-align-c {
-    text-align: center;
-  }
+.italic {
+  font-style: italic;
+}
 
-  .margin-0-auto {
-    margin: 0 auto;
+.c-secondary {
+  color: #f93762;
+}
+
+.c-primary {
+  color: #392283;
+}
+
+.text-align-c {
+  text-align: center;
+}
+
+.margin-0-auto {
+  margin: 0 auto;
+}
+
+.icon-size-resp {
+  width: 45px;
+  height: auto;
+  @media (min-width: map-get($grid-breakpoints, sm)) {
+    width: 55px;
   }
+  @media (min-width: map-get($grid-breakpoints, sm)) {
+    width: 65px;
+  }
+}
+
+.white-space-nw {
+  white-space: nowrap;
+}
+
+.padding-t-30 {
+  padding-top: 30px;
+}
+.padding-r-16 {
+  padding-right: 16px;
+}
+
+.padding-16{
+  padding: 16px;
+}
+
+.cursor-p {
+  cursor: pointer;
+} 
+
+.p-s-resp {
+  font-size: 18px;
+  @media (min-width: map-get($grid-breakpoints, sm)) {
+    font-size: 28px;
+  }
+}
+
+.display-ib {
+  display: inline-block;
+}
+
+.width-0100 {
+  width: 100%
+}
 </style>
