5dfac04028067ebcecdd96cd0dec55fa82a0d9e3
Alvaro Herrero <alvaroherrero@MacBook-Pro-de-Alvaro.local>
2018-01-12 20:14:23 +0100

    correcion bootstrap cabecera

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="bg-header container height-100vh position-r">
     <div class="row height-100vh">
-      <div class="col-xs-12 col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2 vertical-center position-r">
+      <div class="col-xs-10 col-xs-offset-1 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 col-lg-4 col-lg-offset-4 vertical-center position-r">
         <p class="sentence">
           {{sentence.text}}
         </p>
